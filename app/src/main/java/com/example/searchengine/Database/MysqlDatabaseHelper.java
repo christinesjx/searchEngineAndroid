7f4a74ccc6103aefa81086ac59664d6693b6e049
Christine Sun <jxsun95@bu.edu>
2019-12-11 11:05:34 -0500

    fix mysql

--- app/src/main/java/com/example/searchengine/Database/MysqlDatabaseHelper.java
+++ app/src/main/java/com/example/searchengine/Database/MysqlDatabaseHelper.java
@@ -13,13 +13,12 @@ import androidx.annotation.Nullable;
 
 public class MysqlDatabaseHelper extends SQLiteOpenHelper{
 
-
-    public static final String SMARTWATCH_DATABASE = "smartwatch";
-    public static final String ACTIVFIT_TABLE = "activfit";
-    public static final String ACTIVITY_TABLE = "activity";
-    public static final String BATTERY_TABLE = "batterysensor";
-    public static final String BLUETOOTH_TABLE = "bluetooth";
-    public static final String HEARTRATE_TABLE = "heartrate";
+    private static final String SMARTWATCH_DATABASE = "smartwatch";
+    private static final String ACTIVFIT_TABLE = "activfit";
+    private static final String ACTIVITY_TABLE = "activity";
+    private static final String BATTERY_TABLE = "batterysensor";
+    private static final String BLUETOOTH_TABLE = "bluetooth";
+    private static final String HEARTRATE_TABLE = "heartrate";
 
 
     public MysqlDatabaseHelper(@Nullable Context context) {
@@ -45,25 +44,6 @@ public class MysqlDatabaseHelper extends SQLiteOpenHelper{
         onCreate(db);
     }
 
-    /**
-     * get tables' name of this db
-     */
-    public void getAllTables(){
-        ArrayList<String> tableNames = new ArrayList<String>();
-
-        SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = db.rawQuery("SELECT name FROM sqlite_master WHERE type='table'", null);
-        while(cursor.moveToNext()){
-            String tableName = cursor.getString(0);
-            if(tableName.equals("android_metadata")){
-                continue;
-            }else{
-                tableNames.add(tableName);
-            }
-        }
-
-        System.out.println(Arrays.toString(tableNames.toArray()));
-    }
 
     /**
      * search data, based on the table, field, and value
@@ -74,7 +54,7 @@ public class MysqlDatabaseHelper extends SQLiteOpenHelper{
      */
     public Cursor getData(String table, String field, String value) {
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor res = db.rawQuery("select * from "+table +" where " + field + " like " + value,null);
+        Cursor res = db.rawQuery("select * from "+table +" where " + field + " like '%"+value+"%'",null);
         return res;
     }
 
